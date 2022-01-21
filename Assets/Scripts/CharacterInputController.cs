dfaca84c600a94973033e894fd3c7cf827dc9526
Shence <maoyuxuan129@gmail.com>
2022-01-21 23:41:42 +0800

    Add Tilemap, code basic framework

---
+++ Assets/Scripts/CharacterInputController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ns
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class CharacterInputController : MonoBehaviour
+	{
+		private CharacterMotor motor;
+
+        private void Start()
+        {
+			motor = GetComponent<CharacterMotor>();
+        }
+
+        private void Update()
+        {
+            MovementControlDetection();
+            FlashlightControlDetection();
+        }
+
+        private void MovementControlDetection()
+        {
+
+        }
+
+        private void FlashlightControlDetection()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ns
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class CharacterMotor : MonoBehaviour
+	{
+		public void Movement(Vector2 dir)
+        {
+
+        }
+	}
+}
