aee7ca20c6dd7da2416dcc14e326c76e846b9c25
hiukongDan <ifsoboy9148@hotmail.com>
2022-01-25 22:45:00 +0800

    add door data to tilemapData

--- Assets/Hiukong/Script/TilemapFlag.cs
+++ Assets/Hiukong/Script/TilemapFlag.cs
@@ -7,6 +7,7 @@ public class TilemapFlag : MonoBehaviour
     public enum FlagItem
     {
         TwoTile,
+        Door,
         COUNT
     };
 
@@ -15,15 +16,21 @@ public class TilemapFlag : MonoBehaviour
     [Tooltip("Is this tilemap containing two tile high objects")]
     public bool IsTwoTileHigh = false;
 
+    [SerializeField]
+    [Tooltip("Is this tilemap containing doors?")]
+    public bool IsDoors = false;
+
     public void ClearFlag()
     {
         IsTwoTileHigh = false;
+        IsDoors = false;
     }
 
     public void CopyFlag(TilemapFlag other)
     {
         if (other == null) return;
         this.IsTwoTileHigh = other.IsTwoTileHigh;
+        this.IsDoors = other.IsDoors;
     }
     
 }
